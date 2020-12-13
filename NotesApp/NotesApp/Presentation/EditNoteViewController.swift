86ed2d60153be9cd8cd369f9c9b3178ee7cbc009
Joseph <yusuf.fayzullin@gmail.com>
2020-12-13 14:45:22 +0100

    Initial commit

--- NotesApp/NotesApp/Presentation/EditNoteViewController.swift
+++ NotesApp/NotesApp/Presentation/EditNoteViewController.swift
@@ -14,6 +14,9 @@ class EditNoteViewController: UIViewController {
     @IBOutlet private var titleTextField: UITextField!
     @IBOutlet private var textView: UITextView!
 
+    private let noteService = NotesService()
+    var notesViewDelegate: NotesViewDelegate?
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,8 +24,36 @@ class EditNoteViewController: UIViewController {
         textView.layer.borderWidth = 0.5
         textView.layer.borderColor = UIColor.lightGray.cgColor
         textView.layer.cornerRadius = 8
+
+        if let note = note {
+            self.textView.text = note.text
+            self.titleTextField.text = note.title
+        }
     }
 
     @objc private func saveTap(_ sender: Any) {
+        if textView.text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty && ((titleTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty) != nil) {
+            if note != nil {
+                if textView.text.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty && ((titleTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty) != nil) {
+
+                    noteService.notes { result in
+                        switch result {
+                        case .success(_):
+                            self.notesViewDelegate?.deleteNote(note: self.note!, indexPath: nil)
+                        case .failure(_):
+                            print("Failed To Fetch Notes")
+                        }
+                    }
+                }
+            }
+        } else {
+            if note == nil {
+                let note = Note(title: titleTextField.text!, text: textView.text!)
+                noteService.save(note: note, completionHandler: nil)
+                self.notesViewDelegate?.addNote(note: note)
+            }
+        }
+
+        self.navigationController?.popViewController(animated: true)
     }
 }
