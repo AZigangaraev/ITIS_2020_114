a1166275c1561165aafd359ac18013567bfb17bc
Rustem <ironrustem@gmail.com>
2020-12-13 17:16:52 +0300

    with 2 tasks

--- NotesApp/NotesApp/Model/NotesService.swift
+++ NotesApp/NotesApp/Model/NotesService.swift
@@ -8,15 +8,113 @@
 import Foundation
 
 class NotesService {
+    
+    enum NotesServiceError: Error {
+        case notFound
+    }
+    
     /// Получение списка всех заметок.
     func notes(completionHandler: @escaping (Result<[Note], Error>) -> Void) {
+        let documentsDirectoryUrl = FileManager.default.urls(
+            for: .documentDirectory, in: .userDomainMask).first!
+        
+        var fileListArray: [Note] = []
+        do
+          {
+            let fileList = try FileManager.default.contentsOfDirectory(atPath: documentsDirectoryUrl.path)
+            print("fileList\(fileList)")
+              for file in fileList {
+                let DocumentUrl = documentsDirectoryUrl.appendingPathComponent("\(file)")
+                print("url: \(DocumentUrl)")
+                let text2 = try String(contentsOf: DocumentUrl, encoding: .utf8)
+                print("text: \(text2)")
+                let decoder = JSONDecoder()
+                let object = try? decoder.decode(Note.self, from: text2.data(using: .utf8)!)
+                fileListArray.append(object!)
+              }
+            
+//            сортируем по дате изменения
+            fileListArray.sort { (first, second) -> Bool in
+                if first.dateModified! > second.dateModified! {
+                    return true
+                }
+                return false
+            }
+            completionHandler(.success(fileListArray))
+          }
+          catch
+          {
+            completionHandler(.failure(NotesServiceError.notFound))
+          }
     }
 
     /// Создаёт новую заметку, если note.id == nil; Редактирует существующую заметку, если note.id != nil.
     func save(note: Note, completionHandler: @escaping (Result<Void, Error>) -> Void) {
+        print("1")
+
+        let encoder = JSONEncoder()
+        let encodedData = try! encoder.encode(note)
+
+        guard
+            let documentsDirectoryUrl = FileManager.default.urls(
+                for: .documentDirectory, in: .userDomainMask).first
+        else {
+            print("Could not create documents directory url")
+            return
+        }
+
+        print("noteID: \(note.id)")
+        let documentUrl = documentsDirectoryUrl.appendingPathComponent("\(note.id).json")
+        
+//    проверка есть ли еще
+        let g  = try? documentUrl.checkResourceIsReachable()
+           
+        var pined = false;
+        
+        if(g != nil){
+//            если изменяем, то сначала удаляем
+            pined = NotesViewController().containsPined(x: note.id.uuidString)
+            delete(note: note) { (_) in
+            }
+            print("изменил")
+        }
+        
+//        создание
+        guard FileManager.default.createFile(atPath: documentUrl.path, contents: encodedData) else {
+            print("Could not create page at url: \(documentUrl)")
+            return
+        }
+        
+        if (pined){ NotesViewController().addPined(x: note.id.uuidString)}
+        
+        completionHandler(.success(()))
+
     }
 
     /// Удаление заметки.
     func delete(note: Note, completionHandler: @escaping (Result<Void, Error>) -> Void) {
+        
+        if (UserDefaults.standard.array(forKey: "pined") != nil) {
+            if(NotesViewController().containsPined(x: note.id.uuidString)){
+                NotesViewController().removePined(x: note.id.uuidString)
+            }
+        }
+        
+        guard
+            let documentsDirectoryUrl = FileManager.default.urls(
+                for: .documentDirectory, in: .userDomainMask).first
+        else {
+            print("Could not create documents directory url")
+            return
+        }
+
+        print("noteID: \(note.id)")
+        let documentUrl = documentsDirectoryUrl.appendingPathComponent("\(note.id).json")
+        
+
+        try? FileManager.default.removeItem(at: documentUrl)
+        
+        completionHandler(.success(()))
+        
     }
 }
--- NotesApp/NotesApp/Presentation/NotesViewController.swift
+++ NotesApp/NotesApp/Presentation/NotesViewController.swift
@@ -8,24 +8,40 @@
 import UIKit
 
 class NotesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
+    
+    let keyUserDefauls = "pined"
+    @IBOutlet weak var tableView: UITableView!
+    var userDefaults = UserDefaults.standard
+    private var notes: [Note] = []
+    private let cellIdentifier = "Cell"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        если нет, то создаем пустой массив для пиновки, чтобы в дальнейшем не выходили ошибки
+        if (self.userDefaults.array(forKey: keyUserDefauls) == nil) {
+            print("news")
+            let g:[String] = []
+            self.userDefaults.set(g, forKey: keyUserDefauls)
+        }
+        refresh()
     }
+    
 
+//    добавление
     @IBAction func addTap(_ sender: Any) {
         let controller = editNoteController()
         navigationController?.pushViewController(controller, animated: true)
     }
 
-    private var notes: [Note] = []
 
+// сколько всего ячеек
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         notes.count
     }
 
-    private let cellIdentifier = "Cell"
-
+    
+//    как выглядит ячейка
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let note = notes[indexPath.row]
         if let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) {
@@ -34,17 +50,145 @@ class NotesViewController: UIViewController, UITableViewDataSource, UITableViewD
             return cell
         } else {
             let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
+            print("new")
+            cell.textLabel?.text = note.title
+            cell.detailTextLabel?.text = note.text
             return cell
         }
     }
 
+//    переход на детальный обзор
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let controller = editNoteController()
+        controller.note = notes[indexPath.row]
+        controller.mainViewController = self
+        navigationController?.pushViewController(controller, animated: true)
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+//    обновление всех данных на главной странице
+    func refresh(){
+        NotesService().notes() { result in
+            switch result {
+            case .success(let notes):
+                self.notes = notes
+                print("notes: \(self.notes)")
+            case .failure(_):
+                fatalError()
+            }
+        }
+//        это сделано, чтобы вывести запиненные наверх
+        var g:[Note] = []
+        var g1:[Note] = []
+        var j = 0
+        print("notes: \(notes)")
+        print("notesC: \(notes.count)")
+        var forDeleteItems:[Int] = []
+        for i in notes{
+            print("j: \(j)")
+            let id = i.id.uuidString
+            if(containsPined(x: id)){
+                forDeleteItems.append(j)
+                g.append(i)
+            } else {
+                g1.append(i)
+            }
+            j = j + 1
+        }
 
+        notes = g + g1
+        tableView.reloadData()
+    }
+
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+//    свайп для пина
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let note = notes[indexPath.row]
+        let id = note.id.uuidString
+        if containsPined(x: id){
+//            если уже запинен, и нужно распинить
+            let filterAction = UIContextualAction(style: .normal, title: "Unpin") { (action, view, bool) in
+                self.removePined(x: id)
+                self.refresh()
+               }
+            filterAction.backgroundColor = UIColor.gray
+            let configuration = UISwipeActionsConfiguration(actions: [filterAction])
+            configuration.performsFirstActionWithFullSwipe = false
+                return configuration
+        } else {
+//            запинивание
+            let filterAction = UIContextualAction(style: .normal, title: "Pin") { (action, view, bool) in
+                self.addPined(x: id)
+                self.refresh()
+               }
+            filterAction.backgroundColor = UIColor.blue
+            let configuration = UISwipeActionsConfiguration(actions: [filterAction])
+            configuration.performsFirstActionWithFullSwipe = false
+                return configuration
+        }
+       
+    }
+    
+//    функция для проверки запиненности
+    func containsPined(x: String) -> Bool{
+        let array = self.userDefaults.array(forKey: keyUserDefauls) as! [String]
+        for i in array{
+            if(i == x) {
+                return true
+            }
+        }
+        return false
+    }
+    
+//    функция удаления запиненности
+    func removePined(x: String){
+        print("remmove pin")
+        var array = self.userDefaults.array(forKey: keyUserDefauls) as! [String]
+        var j = 0
+        for i in array{
+            if(i == x) {
+                array.remove(at: j)
+            }
+            j = j + 1
+        }
+        self.userDefaults.set(array, forKey: keyUserDefauls)
+    }
+    
+//    добавление запиненности
+    func addPined(x: String){
+        print("add pin: \(x)")
+        var array = UserDefaults.standard.array(forKey: self.keyUserDefauls) as! [String]
+        array.append(x)
+        UserDefaults.standard.setValue(array, forKey: self.keyUserDefauls)
+    }
+    
+
+//    удаление ячеек свайпом
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == .delete) {
+            let note = notes[indexPath.row]
+            NotesService().delete(note: note , completionHandler: { result in
+                switch result {
+                case .success( _):
+                    print("удалилось")
+                    self.refresh()
+                case .failure(_):
+                    fatalError()
+                }
+            })
+        }
+    }
+    
+    
+//    достать детальный контроллер
     private func editNoteController() -> EditNoteViewController {
         guard let storyboard = storyboard else { fatalError() }
-
-        return storyboard.instantiateViewController(identifier: "EditNoteViewController")
+        let editViewController = storyboard.instantiateViewController(identifier: "EditNoteViewController") as! EditNoteViewController
+        editViewController.mainViewController = self
+        return  editViewController
     }
 }
