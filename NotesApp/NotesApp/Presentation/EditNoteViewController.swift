21576a9ac813a5bd926d5b67ee4c4d343e0cf7f6
Pasandep <pasandep@yandex.ru>
2020-12-13 14:29:02 +0300

    Hw14_Lyapustin

--- NotesApp/NotesApp/Presentation/EditNoteViewController.swift
+++ NotesApp/NotesApp/Presentation/EditNoteViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class EditNoteViewController: UIViewController {
     var note: Note?
+    private var notesService = NotesService()
 
     @IBOutlet private var modifiedDateLabel: UILabel!
     @IBOutlet private var titleTextField: UITextField!
@@ -21,8 +22,70 @@ class EditNoteViewController: UIViewController {
         textView.layer.borderWidth = 0.5
         textView.layer.borderColor = UIColor.lightGray.cgColor
         textView.layer.cornerRadius = 8
+        
+        guard let note = note else { return }
+        titleTextField.text = note.title
+        textView.text = note.text
+        guard let date = note.dateModified else { return }
+        modifiedDateLabel.text = "\(String(describing: date))"
+        
     }
 
     @objc private func saveTap(_ sender: Any) {
+        guard var title = titleTextField.text else { return }
+        guard var text = textView.text else { return }
+        title = title.trimmingCharacters(in: [" "])
+        text = text.trimmingCharacters(in: [" "])
+        
+        if var note = note {
+            if edited() {
+                note.title = title
+                note.text = text
+                if (text == "" && title == "") {
+                    notesService.delete(note: note) { result in
+                        switch result {
+                        case .success():
+                            print("Succes")
+                        case .failure(let error):
+                            print(error)
+                        }
+                    }
+                } else {
+                    notesService.save(note: note) { result in
+                        switch result {
+                        case .success():
+                            print("Succes")
+                        case .failure(let error):
+                            print(error)
+                        }
+                    }
+                }
+            }
+        } else {
+            if (title != "" || text != "") {
+                let note = Note(title: title, text: text)
+                notesService.save(note: note) { result in
+                    switch result {
+                    case .success():
+                        print("Succes")
+                    case .failure(let error):
+                        print(error)
+                    }
+                }
+            }
+        }
+        navigationController?.popViewController(animated: true)
+    }
+    
+    private func edited() -> Bool {
+        guard let note = note else { return false }
+        let title = note.title
+        let text = note.text
+        
+        if (title == titleTextField.text && text == textView.text) {
+            return false
+        }
+        
+        return true
     }
 }
--- NotesApp/NotesApp/Presentation/NotesViewController.swift
+++ NotesApp/NotesApp/Presentation/NotesViewController.swift
@@ -9,8 +9,30 @@ import UIKit
 
 class NotesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet var notesTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadData()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        loadData()
+    }
+    
+    private func loadData() {
+        notesService.notes { result in
+            switch result {
+                case .success(let notesFiller):
+                    self.notes = notesFiller
+                case .failure(let error):
+                    print(error)
+            }
+        }
+        countOfPinnedNotes = getCountOfPinnedNotes()
+        notesTableView.reloadData()
     }
 
     @IBAction func addTap(_ sender: Any) {
@@ -19,6 +41,10 @@ class NotesViewController: UIViewController, UITableViewDataSource, UITableViewD
     }
 
     private var notes: [Note] = []
+    private var notesService = NotesService()
+    var countOfPinnedNotes = 0
+    var userDefault = UserDefaults.standard
+    private var pinnedNotesKey = "pinnedNotesKey"
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         notes.count
@@ -31,20 +57,90 @@ class NotesViewController: UIViewController, UITableViewDataSource, UITableViewD
         if let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) {
             cell.textLabel?.text = note.title
             cell.detailTextLabel?.text = note.text
+            cell.accessoryType = .none
+            if (indexPath.row < countOfPinnedNotes) {
+                cell.accessoryType = .checkmark
+            }
             return cell
         } else {
             let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
+            cell.textLabel?.text = note.title
+            cell.detailTextLabel?.text = note.text
+            cell.accessoryType = .none
+            if (indexPath.row < countOfPinnedNotes) {
+                cell.accessoryType = .checkmark
+            }
             return cell
         }
+    }
+    
+    func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        if (indexPath.row < countOfPinnedNotes) {
+            let action = UIContextualAction(style: .normal, title: "Unpin")
+            { [weak self] (action, view, completionHandler) in
+                self?.unpinNoteTapped(rowAt: indexPath.row)
+                completionHandler(true)
+            }
+            
+            action.backgroundColor = .systemOrange
+            return UISwipeActionsConfiguration(actions: [action])
+        } else {
+            let action = UIContextualAction(style: .normal, title: "Pin")
+            { [weak self] (action, view, completionHandler) in
+                self?.pinNoteTapped(rowAt: indexPath.row)
+                completionHandler(true)
+            }
+            
+            action.backgroundColor = .systemOrange
+            return UISwipeActionsConfiguration(actions: [action])
+        }
+    }
+    
+    private func pinNoteTapped(rowAt row: Int) {
+        notesService.pinNote(note: notes[row])
+        loadData()
+    }
+    
+    private func unpinNoteTapped(rowAt row: Int) {
+        notesService.unpinNote(note: notes[row])
+        loadData()
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            notesService.delete(note: notes[indexPath.row]) { result in
+                switch result {
+                case .success():
+                    print("Succes")
+                case .failure(let error):
+                    print(error)
+                }
+            }
+        }
+        loadData()
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        
+        let editNoteViewController = editNoteController()
+        
+        editNoteViewController.note = notes[indexPath.row]
+        show(editNoteViewController, sender: nil)
     }
 
     private func editNoteController() -> EditNoteViewController {
         guard let storyboard = storyboard else { fatalError() }
 
         return storyboard.instantiateViewController(identifier: "EditNoteViewController")
+    }
+    
+    private func getCountOfPinnedNotes() -> Int {
+        let pinnedNotesId = userDefault.stringArray(forKey: pinnedNotesKey) ?? [String]()
+        return pinnedNotesId.count
     }
 }
