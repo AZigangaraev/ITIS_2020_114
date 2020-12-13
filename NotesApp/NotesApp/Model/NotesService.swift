465356b1c688ea47e4bf554c686db0fdc2448567
Rishat <rishatl@inbox.ru>
2020-12-13 16:28:58 +0300

    Latypov_Rishat_hw11
    Сделал доп. задание только на 2 балла.

--- NotesApp/NotesApp/Model/NotesService.swift
+++ NotesApp/NotesApp/Model/NotesService.swift
@@ -8,15 +8,66 @@
 import Foundation
 
 class NotesService {
-    /// Получение списка всех заметок.
-    func notes(completionHandler: @escaping (Result<[Note], Error>) -> Void) {
+    private let fileManager: FileManager
+    private let path: URL
+    private let decoder: JSONDecoder
+    private let encoder: JSONEncoder
+    
+    init() {
+        fileManager = FileManager()
+        path = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+        decoder = JSONDecoder()
+        encoder = JSONEncoder()
     }
-
+    
+    private func encode(note: Note) -> Data{
+        guard let data = try? encoder.encode(note) else {
+            fatalError()
+        }
+        return data
+    }
+    
+    private func decode(data: Data) -> Note{
+        if let note = try? decoder.decode(Note.self, from: data) {
+            return note
+        }else {
+            fatalError()
+        }
+    }
+    
+    /// Получение списка всех заметок.
+    func notes (completionHandler: @escaping (Result<[Note], Error>) -> Void) {
+        var notes = [Note]()
+        var sortedNotes = [Note]()
+        do {
+            for folderPath in try fileManager.contentsOfDirectory(atPath: path.path) {
+                let filePath = path.appendingPathComponent(folderPath)
+                guard let data = fileManager.contents(atPath: filePath.path) else {
+                    fatalError()
+                }
+                let note = decode(data: data)
+                notes.append(note)
+            }
+            let sortNotes = notes.sorted { (firstNote, secondNote) -> Bool in
+                return firstNote.dateModified ?? Date() > secondNote.dateModified ?? Date()
+            }
+            sortedNotes.append(contentsOf: sortNotes)
+            
+        }catch {
+            completionHandler(.failure(error))
+        }
+        completionHandler(.success(sortedNotes))
+    }
+    
     /// Создаёт новую заметку, если note.id == nil; Редактирует существующую заметку, если note.id != nil.
     func save(note: Note, completionHandler: @escaping (Result<Void, Error>) -> Void) {
+        let filePath = path.appendingPathComponent("\(note.id).json")
+        fileManager.createFile(atPath: filePath.path, contents: encode(note: note), attributes: nil)
     }
-
+    
     /// Удаление заметки.
     func delete(note: Note, completionHandler: @escaping (Result<Void, Error>) -> Void) {
+        let filePath = path.appendingPathComponent("\(note.id).json")
+        try? fileManager.removeItem(atPath: filePath.path)
     }
 }
--- NotesApp/NotesApp/Presentation/EditNoteViewController.swift
+++ NotesApp/NotesApp/Presentation/EditNoteViewController.swift
@@ -9,7 +9,8 @@ import UIKit
 
 class EditNoteViewController: UIViewController {
     var note: Note?
-
+    private let service = NotesService()
+    
     @IBOutlet private var modifiedDateLabel: UILabel!
     @IBOutlet private var titleTextField: UITextField!
     @IBOutlet private var textView: UITextView!
@@ -21,8 +22,85 @@ class EditNoteViewController: UIViewController {
         textView.layer.borderWidth = 0.5
         textView.layer.borderColor = UIColor.lightGray.cgColor
         textView.layer.cornerRadius = 8
+        
+        loadNote()
     }
 
+    private func loadNote() {
+        guard let note = note else {
+            return
+        }
+        guard let date = note.dateModified else {
+            return
+        }
+        modifiedDateLabel.text = "\(date)"
+        titleTextField.text = note.title
+        textView.text = note.text
+    }
+    
     @objc private func saveTap(_ sender: Any) {
+        guard var title = titleTextField.text else { fatalError() }
+        guard var text = textView.text else { fatalError() }
+        text = text.trimmingCharacters(in: [" "])
+        title = title.trimmingCharacters(in: [" "])
+        
+        if var note = self.note {
+            note.title = title
+            note.text = text
+            note.dateModified = Date()
+            
+            service.delete(note: note) { (result) in
+                switch result {
+                case .success():
+                    print("Deleted")
+                case .failure(let error):
+                    print(error)
+                }
+            }
+            if note.title != "" {
+                service.save(note: note) { (result) in
+                    switch result {
+                    case .success():
+                        print("Saved")
+                    case .failure(let error):
+                        print(error)
+                    }
+                }
+            } else if note.title == "" && note.text != ""{
+                note.title = "No title"
+                service.save(note: note) { (result) in
+                    switch result {
+                    case .success():
+                        print("Saved")
+                    case .failure(let error):
+                        print(error)
+                    }
+                }
+            }
+        } else {
+            if title != ""{
+                let note = Note(title: title, text: text, dateModified: Date())
+                service.save(note: note) { (result) in
+                    switch result {
+                    case .success():
+                        print("Saved")
+                    case .failure(let error):
+                        print(error)
+                    }
+                }
+            } else if title == "" && text != ""{
+                title = "No title"
+                let note = Note(title: title, text: text, dateModified: Date())
+                service.save(note: note) { (result) in
+                    switch result {
+                    case .success():
+                        print("Saved")
+                    case .failure(let error):
+                        print(error)
+                    }
+                }
+            }
+        }
+        navigationController?.popViewController(animated: true)
     }
 }
