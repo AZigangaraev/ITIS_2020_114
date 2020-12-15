27a61e3e203e3b41d1258a4438dbf331d1ed8199
oleg-romanov <55031494+oleg-romanov@users.noreply.github.com>
2020-12-15 21:50:05 +0300

    homework

--- NotesApp/NotesApp/Presentation/EditNoteViewController.swift
+++ NotesApp/NotesApp/Presentation/EditNoteViewController.swift
@@ -18,11 +18,33 @@ class EditNoteViewController: UIViewController {
         super.viewDidLoad()
 
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saveTap))
+        textView.text = note?.text
+        titleTextField.text = note?.title
         textView.layer.borderWidth = 0.5
         textView.layer.borderColor = UIColor.lightGray.cgColor
         textView.layer.cornerRadius = 8
     }
 
     @objc private func saveTap(_ sender: Any) {
+        guard var note = note else {
+            return
+        }
+        note.dateModified = Date()
+        note.text = textView.text
+        note.title = titleTextField.text ?? ""
+        NotesService.shared.save(note: note) { [weak self] result in
+            switch result {
+            case .success:
+                self?.navigationController?.popViewController(animated: true)
+            case let .failure(error):
+                self?.showError(message: error.localizedDescription)
+            }
+        }
+    }
+    
+    private func showError(message: String) {
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
 }
--- NotesApp/NotesApp/Presentation/NotesViewController.swift
+++ NotesApp/NotesApp/Presentation/NotesViewController.swift
@@ -9,12 +9,33 @@ import UIKit
 
 class NotesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.tableFooterView = UIView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        loadNotes()
+    }
+    
+    private func loadNotes() {
+        NotesService.shared.notes { [weak self] result in
+            switch result {
+            case let .success(data):
+                self?.notes = data
+                self?.tableView.reloadData()
+            case let .failure(error):
+                self?.showError(message: error.localizedDescription)
+            }
+        }
     }
 
     @IBAction func addTap(_ sender: Any) {
         let controller = editNoteController()
+        controller.note = Note(title: "", text: "")
         navigationController?.pushViewController(controller, animated: true)
     }
 
@@ -28,23 +49,44 @@ class NotesViewController: UIViewController, UITableViewDataSource, UITableViewD
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let note = notes[indexPath.row]
-        if let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) {
-            cell.textLabel?.text = note.title
-            cell.detailTextLabel?.text = note.text
-            return cell
-        } else {
-            let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
-            return cell
-        }
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
+        cell.textLabel?.text = note.title
+        cell.detailTextLabel?.text = note.text
+        return cell
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let controller = editNoteController()
+        controller.note = notes[indexPath.row]
+        navigationController?.pushViewController(controller, animated: true)
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        let note = notes[indexPath.row]
+        NotesService.shared.delete(note: note) { [weak self] result in
+            switch result {
+            case .success:
+                self?.notes.remove(at: indexPath.row)
+                tableView.reloadData()
+            case let .failure(error):
+                self?.showError(message: error.localizedDescription)
+            }
+        }
     }
 
     private func editNoteController() -> EditNoteViewController {
         guard let storyboard = storyboard else { fatalError() }
-
         return storyboard.instantiateViewController(identifier: "EditNoteViewController")
+    }
+    
+    private func showError(message: String) {
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
 }
