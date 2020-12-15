5ba28c710fb380d68886462a9fab1725def91404
evelina <yangirova01@bk.ru>
2020-12-15 15:29:48 +0300

    a

--- NotesApp/NotesApp/Model/NotesService.swift
+++ NotesApp/NotesApp/Model/NotesService.swift
@@ -7,16 +7,99 @@
 
 import Foundation
 
-class NotesService {
-    /// Получение списка всех заметок.
-    func notes(completionHandler: @escaping (Result<[Note], Error>) -> Void) {
-    }
+ class NotesService {
+     private let fileManager: FileManager
+     private let path: URL
+     private let decoder: JSONDecoder
+     private let encoder: JSONEncoder
 
-    /// Создаёт новую заметку, если note.id == nil; Редактирует существующую заметку, если note.id != nil.
-    func save(note: Note, completionHandler: @escaping (Result<Void, Error>) -> Void) {
-    }
+     init() {
+         fileManager = FileManager()
+         path = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
+         decoder = JSONDecoder()
+         encoder = JSONEncoder()
+     }
+     func notes(completionHandler: @escaping (Result<[Note], Error>) -> Void) {
+         var notes = [Note]()
+         var pinNotes = [Note]()
+         var allNotes = [Note]()
+         guard let listN = UserDefaults.standard.stringArray(forKey: "array") else {
+             return
+         }
+         do {
+             for string in try fileManager.contentsOfDirectory(atPath: path.path) {
+                 let filePath = path.appendingPathComponent(string)
+                 guard let data = fileManager.contents(atPath: filePath.path) else {
+                     fatalError()
+                 }
+                 let note = decode(data: data)
+                 if listN.contains(note.id.uuidString) {
+                     pinNotes.append(note)
+                 }else {
+                     notes.append(note)
+                 }
+             }
+             let sortedNotes = notes.sorted { (note1, note2) -> Bool in
+                 return note1.dateModified ?? Date() > note2.dateModified ?? Date()
+             }
+             allNotes.append(contentsOf: pinNotes)
+             allNotes.append(contentsOf: sortedNotes)
 
-    /// Удаление заметки.
-    func delete(note: Note, completionHandler: @escaping (Result<Void, Error>) -> Void) {
-    }
-}
+         } catch {
+             completionHandler(.failure(error))
+         }
+         completionHandler(.success(allNotes))
+     }
+
+     func save(note: Note, completionHandler: @escaping (Result<Void, Error>) -> Void) {
+         let filePath = path.appendingPathComponent("\(note.id).json")
+         fileManager.createFile(atPath: filePath.path, contents: encode(note: note), attributes: nil)
+     }
+    
+     func delete(note: Note, completionHandler: @escaping (Result<Void, Error>) -> Void) {
+         let filePath = path.appendingPathComponent("\(note.id).json")
+         try? fileManager.removeItem(atPath: filePath.path)
+     }
+
+     func pin(note: Note, compleationHandler: @escaping (Result<Void, Error>) -> Void) {
+         guard var list = UserDefaults.standard.stringArray(forKey: "array") else {
+             return
+         }
+         list.append(note.id.uuidString)
+         UserDefaults.standard.setValue(list, forKey: "array")
+     }
+
+
+     func unPin(id: Int, compleationHandler: @escaping (Result<Void, Error>) -> Void) {
+         guard var list = UserDefaults.standard.stringArray(forKey: "array") else {
+             return
+         }
+         if id < list.count {
+             list.remove(at: id)
+             UserDefaults.standard.setValue(list, forKey: "array")
+         }
+     }
+
+     private func encode(note: Note) -> Data{
+         guard let data = try? encoder.encode(note) else {
+             fatalError()
+         }
+         return data
+     }
+
+     private func decode(data: Data) -> Note{
+         if let note = try? decoder.decode(Note.self, from: data) {
+             return note
+         } else {
+             fatalError()
+         }
+     }
+
+     func checkPinedNote(note: Note) -> Bool {
+         guard let list = UserDefaults.standard.stringArray(forKey: "array") else {
+             return false
+         }
+
+         return list.contains(note.id.uuidString)
+     }
+ }
--- NotesApp/NotesApp/Presentation/NotesViewController.swift
+++ NotesApp/NotesApp/Presentation/NotesViewController.swift
@@ -7,44 +7,157 @@
 
 import UIKit
 
-class NotesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+ class NotesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
+     private let service = NotesService()
 
-    @IBAction func addTap(_ sender: Any) {
-        let controller = editNoteController()
-        navigationController?.pushViewController(controller, animated: true)
-    }
+     override func viewDidLoad() {
+         super.viewDidLoad()
+         loadData()
+     }
 
-    private var notes: [Note] = []
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        notes.count
-    }
+     override func viewWillAppear(_ animated: Bool) {
+         super.viewWillAppear(animated)
+         loadData()
+     }
 
-    private let cellIdentifier = "Cell"
+     @IBAction func addTap(_ sender: Any) {
+         let controller = editNoteController()
+         navigationController?.pushViewController(controller, animated: true)
+     }
 
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let note = notes[indexPath.row]
-        if let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) {
-            cell.textLabel?.text = note.title
-            cell.detailTextLabel?.text = note.text
-            return cell
-        } else {
-            let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
-            return cell
-        }
-    }
+     
+     private var notes: [Note] = []
 
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-    }
+     @IBOutlet weak var tableView: UITableView!
 
-    private func editNoteController() -> EditNoteViewController {
-        guard let storyboard = storyboard else { fatalError() }
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         notes.count
+     }
 
-        return storyboard.instantiateViewController(identifier: "EditNoteViewController")
-    }
-}
+     
+     private let cellIdentifier = "Cell"
+
+     
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         let note = notes[indexPath.row]
+         if let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) {
+             cell.textLabel?.text = note.title
+             cell.detailTextLabel?.text = note.text
+             cell.accessoryType = .none
+             if self.service.checkPinedNote(note: note) {
+                 cell.accessoryType = .checkmark
+             }
+             return cell
+         } else {
+             let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
+             cell.textLabel?.text = note.title
+             cell.detailTextLabel?.text = note.text
+             if self.service.checkPinedNote(note: note) {
+                 cell.accessoryType = .checkmark
+             }
+             return cell
+         }
+     }
+
+     
+     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+         tableView.deselectRow(at: indexPath, animated: true)
+         let controller = editNoteController()
+         controller.note = notes[indexPath.row]
+         navigationController?.pushViewController(controller, animated: true)
+     }
+
+     
+     private func editNoteController() -> EditNoteViewController {
+         guard let storyboard = storyboard else { fatalError() }
+
+         
+         return storyboard.instantiateViewController(identifier: "EditNoteViewController")
+     }
+
+     private func loadData(){
+         service.notes { (result) in
+             switch result {
+             case .success(let newNotes):
+                 self.notes = newNotes
+             case.failure(let error):
+                 print(error)
+             }
+         }
+         tableView.reloadData()
+     }
+
+     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+         true
+     }
+
+     func tableView(_ tableView: UITableView, leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+         if self.service.checkPinedNote(note: notes[indexPath.row]) {
+             let action = UIContextualAction(style: .normal, title: "Unpin") { [weak self] (action, view, completionHandler) in
+                 self?.service.unPin(id: indexPath.row) { (result) in
+                     switch result {
+                     case .success:
+                         print("Unpined")
+                     case .failure(let error):
+                         print(error)
+                     }
+                 }
+                 self?.loadData()
+             }
+             action.backgroundColor = .systemBlue
+             return UISwipeActionsConfiguration(actions: [action])
+         } else {
+             let action = UIContextualAction(style: .normal, title: "Pin") { [weak self] (action, view, completionHandler) in
+                 guard let note = self?.notes[indexPath.row] else {
+                     return
+                 }
+                 self?.service.pin(note: note) { (result) in
+                     switch result {
+                     case .success:
+                         print("Pined")
+                     case .failure(let error):
+                         print(error)
+                     }
+                 }
+                 self?.loadData()
+             }
+
+             action.backgroundColor = .systemBlue
+             loadData()
+             return UISwipeActionsConfiguration(actions: [action])
+
+         }
+     }
+
+     func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+         let action = UIContextualAction(style: .destructive, title: "Delete") { [weak self] (action, view, compleationHandler) in
+             guard let note = self?.notes[indexPath.row], let service = self?.service else {
+                 return
+             }
+             if service.checkPinedNote(note: note){
+                 service.unPin(id: indexPath.row) { (result) in
+                     switch result {
+                     case .success:
+                         print("Unpin")
+                     case .failure(let error):
+                         print(error)
+                     }
+                 };
+             }
+             service.delete(note: note, completionHandler: { (result) in
+                 switch result {
+                 case .success:
+                     print("Deleted")
+                 case .failure(let error):
+                     print(error)
+                 }
+             });
+             self?.loadData()
+         }
+         action.backgroundColor = .systemRed
+         loadData()
+         return UISwipeActionsConfiguration(actions: [action])
+     }
+ }
